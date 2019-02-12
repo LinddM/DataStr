bad8693ed2d1cd0c8cd3a8071bfc9a22f5880f74
Lindsey Maite <43527122+LinddM@users.noreply.github.com>
2019-02-11 20:21:12 -0600

    Add files via upload

---
+++ struct/coordinates/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Coord
+{
+    class Program
+    {
+        public struct Point
+        {
+            public int x;
+            public int y;
+        }
+        static void Main(string[] args)
+        {
+            Point p;
+
+            Console.Write("X coordinate: ");
+            p.x = int.Parse(Console.ReadLine());
+            
+            Console.Write("Y coordinate: ");
+            p.y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(quadrant(p));
+        }
+
+        public static String quadrant(Point p)
+        {
+            String location="origin";
+            if (p.x > 0)
+            {
+                if (p.y > 0)
+                {
+                    location="quadrant 1";
+                }
+                else
+                {
+                    location="quadrant 4";
+                }
+            }
+            else
+            {
+                if (p.y > 0)
+                {
+                    location="quadrant 2";
+                }
+                else if (p.y < 0)
+                {
+                    location="quadrant 3";
+                }
+            }
+            return location;
+        }
+
+    }
+}
---
+++ struct/triangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Shapes
+{
+    class Program
+    {
+        public struct triangle
+        {
+            public int b;
+            public int h;
+        }
+        static void Main(string[] args)
+        {
+            triangle tri;
+
+            Console.Write("Area of a triangle\nBase: ");
+            tri.b=Int16.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            tri.h=Int16.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Area: {area(tri)}");
+        }
+        public static int area(triangle tri){
+            int a=tri.h*tri.b/2;
+            return a;
+        }
+    }
+}
