a53ab452d5f8f18a88550c371b887eaa24572ee7
Lindsey Maite <43527122+LinddM@users.noreply.github.com>
2019-02-18 22:50:19 -0600

    Add files via upload

---
+++ PlayList/UnitTestMusic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestMusic
+{
+    [TestClass]
+    public class UnitTestPlayList
+    {
+        [TestMethod]
+        public void IsNextSongCorrect() {
+            var songs = new Playlist();
+            song.Main.op = 1;
+            song.Main.newSong = "1. La catedra";
+            song.Main.op = 1;
+            song.Main.newSong = "2. Mis disculpas";
+            Assert.AreEqual(songs.head.next.name, songs.tail.name);
+        }
+
+        [TestMethod]
+        public void IsPrevSongCorrect() {
+            var songs = new Playlist();
+            song.Main.op = 1;
+            song.Main.newSong = "1. Desencuentro";
+            song.Main.op = 1;
+            song.Main.newSong = "2. Querido Louis";
+            Assert.AreEqual(songs.head.prev.name, songs.tail.name);
+        }
+
+        [TestMethod]
+        public void PrintsPlayList() {
+            var songs = new Playlist();
+            String list;
+            for (int i=0; i<3; i++) {
+                song.Main.op = 1;
+                song.Main.newSong = $"Song {i}";
+                list += $"Song {i}\n";
+            }
+            song.Main.op = 5;
+            Console.WriteLine(list);
+        }
+    }
+}
